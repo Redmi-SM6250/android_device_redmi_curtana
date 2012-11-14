3a0bc578ad45ff6b64f8da3ac1f05a51e8f5fa90
Arne Coucheron <arco68@gmail.com>
2012-11-14 03:47:26 +0100

    curtana: Inherit full_base_telephony

--- lineage_curtana.mk
+++ lineage_curtana.mk
@@ -6,6 +6,7 @@
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from curtana device
 $(call inherit-product, device/redmi/curtana/device.mk)
