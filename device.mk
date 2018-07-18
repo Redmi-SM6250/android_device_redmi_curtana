2ee5b90a5c24af1d26dd19f13cf155c2707c80e3
Danny Baumann <dannybaumann@web.de>
2018-07-18 17:25:32 +0200

    curtana: Enable call recording
    Change-Id: I1b4478b330b13cf5d0fe1ed0d40f43e4b3825f33

--- device.mk
+++ device.mk
@@ -87,7 +87,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Overlays - override vendor ones
 PRODUCT_PACKAGES += \
