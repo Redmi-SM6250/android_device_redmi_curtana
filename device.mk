d261a38ac55ff20267c4fe8f4ed85a48b91cc2ed
Bruno Martins <bgcngm@gmail.com>
2019-04-16 10:44:56 +0000

    curtana: Blacklist fingerprint HAL key events
    Change-Id: I57db108f5250c120726b77dd32f4666917c9eb7c
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- device.mk
+++ device.mk
@@ -59,6 +59,11 @@ PRODUCT_COPY_FILES += \
 # Init
 PRODUCT_PACKAGES += \
     init.mi_thermald.rc
+    
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
 
 # Lights
 PRODUCT_PACKAGES += \
