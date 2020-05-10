94e2b3b4c8dd94e3bc2ce3fb73e745b98d3024a7
QuantumShqipe <dr.techlc@gmail.com>
2020-05-10 22:37:43 +0100

    curtana:bringup: Evolution X Ten!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_curtana.mk
+    $(LOCAL_DIR)/aosp_curtana.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_curtana-user \
-    lineage_curtana-userdebug \
-    lineage_curtana-eng
+    aosp_curtana-user \
+    aosp_curtana-userdebug \
+    aosp_curtana-eng
--- aosp_curtana.mk
+++ aosp_curtana.mk
@@ -12,12 +12,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/redmi/curtana/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_curtana
+PRODUCT_NAME := aosp_curtana
 PRODUCT_DEVICE := curtana
-PRODUCT_BRAND := Redmi
+PRODUCT_BRAND := xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 
 # Build info
