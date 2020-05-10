272fe79ce6435dab293afaccc816c5370201ffbc
TheMalachite <eliasgheeraert@gmail.com>
2020-05-10 05:12:35 +0200

    curtana: init: Rework on the init script * Also use stock build fingerprint instead of pixel one (LineageOS Team don't allow this)

--- init/init_curtana.cpp
+++ init/init_curtana.cpp
@@ -1,37 +1,20 @@
 /*
-   Copyright (c) 2015, The Linux Foundation. All rights reserved.
-   Copyright (C) 2016 The CyanogenMod Project.
-   Copyright (C) 2019-2020 The LineageOS Project.
-
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are
-   met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of The Linux Foundation nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-#include <fstream>
-#include <unistd.h>
-#include <vector>
-
+#include <android-base/logging.h>
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
@@ -39,63 +22,40 @@
 #include "property_service.h"
 #include "vendor_init.h"
 
-using android::base::GetProperty;
 using android::init::property_set;
 
-std::vector<std::string> ro_props_default_source_order = {
-    "",
-    "odm.",
-    "product.",
-    "system.",
-    "vendor.",
-};
-
-void property_override(char const prop[], char const value[], bool add = true) {
+void property_override(char const prop[], char const value[])
+{
     prop_info *pi;
 
-    pi = (prop_info *)__system_property_find(prop);
+    pi = (prop_info*) __system_property_find(prop);
     if (pi)
         __system_property_update(pi, value, strlen(value));
-    else if (add)
+    else
         __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+void load_curtanaglobal() {
+    property_override("ro.product.model", "Redmi Note 9S");
+    property_override("ro.product.product.model", "Redmi Note 9S");
+    property_override("ro.product.system.model", "Redmi Note 9S");
+    property_override("ro.build.description", "curtana_global-user 10 QKQ1.191215.002 V11.0.7.0.QJWMIXM release-keys");
+    property_override("ro.build.fingerprint", "Redmi/curtana_global/curtana:10/QKQ1.191215.002/V11.0.7.0.QJWMIXM:user/release-keys");
+}
+
+void load_curtana() {
+    property_override("ro.product.model", "Redmi Note 9 Pro");
+    property_override("ro.product.product.model", "Redmi Note 9 Pro");
+    property_override("ro.product.system.model", "Redmi Note 9 Pro");
+    property_override("ro.build.description", "curtana-user 10 QKQ1.191215.002 V11.0.6.0.QJWINXM release-keys");
+    property_override("ro.build.fingerprint", "Redmi/curtana/curtana:10/QKQ1.191215.002/V11.0.6.0.QJWINXM:user/release-keys");
 }
 
 void vendor_load_properties() {
-    const auto set_ro_build_prop = [](const std::string &source,
-                                      const std::string &prop,
-                                      const std::string &value) {
-        auto prop_name = "ro." + source + "build." + prop;
-        property_override(prop_name.c_str(), value.c_str(), false);
-    };
+    std::string region = android::base::GetProperty("ro.boot.hwc", "");
 
-    const auto set_ro_product_prop = [](const std::string &source,
-                                        const std::string &prop,
-                                        const std::string &value) {
-        auto prop_name = "ro.product." + source + prop;
-        property_override(prop_name.c_str(), value.c_str(), false);
-    };
-
-    std::string region;
-    region = GetProperty("ro.boot.hwc", "");
-
-    if (region == "global_two") {
-        for (const auto &source : ro_props_default_source_order) {
-            set_ro_build_prop(source, "fingerprint",
-                               "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
-            set_ro_product_prop(source, "model", "Redmi Note 9S");
-        }
-        property_override("ro.build.description", "curtana-user 10 QKQ1.191215.002 V11.0.7.0.QJWMIXM release-keys");
+    if (region.find("global_two") != std::string::npos) {
+        load_curtanaglobal();
     } else {
-        for (const auto &source : ro_props_default_source_order) {
-            set_ro_build_prop(source, "fingerprint",
-                              "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
-            set_ro_product_prop(source, "model", "Redmi Note 9 Pro");
-        }
-        property_override("ro.build.description", "curtana-user 10 QKQ1.191215.002 V11.0.5.0.QJWINXM release-keys");
-        property_override("ro.bluetooth.a2dp_offload.supported", "false");
-        property_override("persist.bluetooth.a2dp_offload.disabled", "true");
-        property_override("persist.bluetooth.bluetooth_audio_hal.disabled", "true");
-        property_override("persist.vendor.qcom.bluetooth.enable.splita2dp", "false");
-        property_override("vendor.audio.feature.a2dp_offload.enable", "false");
+        load_curtana();
     }
 }
