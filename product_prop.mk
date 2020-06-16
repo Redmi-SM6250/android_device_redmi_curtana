c8a787938acb06e611e87153c972eab112a9bcca
Ady Abraham <email>
2020-06-16 15:03:51 +0000

    curtana: prop: Enable GL comp backpressure

--- product_prop.mk
+++ product_prop.mk
@@ -70,6 +70,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     debug.egl.hw=0 \
     debug.mdpcomp.logs=0 \
     debug.sf.disable_backpressure=1 \
+    debug.sf.enable_gl_backpressure=1 \
     debug.sf.early_app_phase_offset_ns=1500000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
