b351b84e07caaa68e0942da36642fa73f4838a4f
Nicholas Magill <nmagill1979@gmail.com>
2020-06-18 18:27:34 +0100

    curtana: Enable updateable APEXes
    Requires encrypted storage so enable that too
    
    Change-Id: I37c73c3cdb8bfb719252aa359d42697b986220c6

--- aosp_curtana.mk
+++ aosp_curtana.mk
@@ -7,6 +7,7 @@
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Inherit from curtana device
 $(call inherit-product, device/redmi/curtana/device.mk)
